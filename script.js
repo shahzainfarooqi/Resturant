673d4a619ea266589d0b24a7557f84f5ed38200f
shahzainfarooqi <142692117+shahzainfarooqi@users.noreply.github.com>
2024-10-01 01:22:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Reservation Form Submission
+document.getElementById('reservation-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you! Your reservation is confirmed.');
+});
+
+// Contact Form Submission
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you! Your message has been sent.');
+});
